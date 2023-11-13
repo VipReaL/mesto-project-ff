ce0b6001cacfea4c8e5566b409e8193672524cfb
Sergey Igoshin <VipReaLWeb@gmail.com>
2023-11-13 20:54:58 +0500

    Сделал функцию добавления карточки в разменку

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,35 @@ const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function createCard (imageSrc, imageAlt, nameValue, deleteHandler) {
+function createCard (imageSrc, nameValue, deleteHandler) {
 
   const templateElement = template.querySelector('.card').cloneNode(true);
 
-  templateElement.querySelector('.card__image').src = imageSrc;
-  templateElement.querySelector('.card__image').alt = imageAlt;
+  const image = templateElement.querySelector('.card__image');
+  image.src = imageSrc;
+  image.alt = nameValue;
   templateElement.querySelector('.card__title').textContent = nameValue;
 
   const deleteButton = templateElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', deleteHandler);
 
-  placesList.append(templateElement);
-};
+  return templateElement
+}
+
+// @todo: Функция добавления карточки в разметку
+
+function addCard (markupCard) {
+  placesList.append(markupCard);
+}
 
 // @todo: Функция удаления карточки
 
 function deleteCard (event) {
-  event.target.parentElement.remove();
-};
+  event.target.closest('.card').remove();
+}
 
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach(function (item) {
-  createCard(item.link, item.name, item.name, deleteCard);
+  addCard(createCard(item.link, item.name, deleteCard));
 });
